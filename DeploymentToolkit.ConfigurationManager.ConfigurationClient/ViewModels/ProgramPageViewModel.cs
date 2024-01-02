d7320f9034be1eb886dff1cef344677fa63d5d1d
Gérald Döserich <github@doeseri.ch>
2024-01-02 13:24:36 +0100

    Fixed Copy Pasta

--- DeploymentToolkit.ConfigurationManager.ConfigurationClient/ViewModels/ProgramPageViewModel.cs
+++ DeploymentToolkit.ConfigurationManager.ConfigurationClient/ViewModels/ProgramPageViewModel.cs
@@ -46,7 +46,6 @@ namespace DeploymentToolkit.ConfigurationManager.ConfigurationClient.ViewModels
             App.Current.DispatcherQueue.TryEnqueue(() =>
             {
                 IsLoading = false;
-                Programs.Clear();
             });
         }
     }
