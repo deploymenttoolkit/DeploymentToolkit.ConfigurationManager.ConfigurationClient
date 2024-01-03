b2e50c4d29ad4f46b7fe0ace6c480953448dd43c
Gérald Döserich <github@doeseri.ch>
2024-01-03 19:37:21 +0100

    Hide Buttons in Release Mode

--- src/DeploymentToolkit.ConfigurationManager.ConfigurationClient/ViewModels/CachePageViewModel.cs
+++ src/DeploymentToolkit.ConfigurationManager.ConfigurationClient/ViewModels/CachePageViewModel.cs
@@ -11,207 +11,212 @@ using DeploymentToolkit.ConfigurationManager.ConfigurationClient.Models.CCM.Soft
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 
-namespace DeploymentToolkit.ConfigurationManager.ConfigurationClient.ViewModels
+namespace DeploymentToolkit.ConfigurationManager.ConfigurationClient.ViewModels;
+
+public partial class CachePageViewModel : ObservableObject
 {
-    public partial class CachePageViewModel : ObservableObject
+#if DEBUG
+    public Visibility DebugVisibility { get; set; } = Visibility.Visible;
+#else
+public Visibility DebugVisibility { get; set; } = Visibility.Collapsed;
+#endif
+
+    [ObservableProperty]
+    private bool _isLoading = true;
+
+    public CachePage Page { get; set; }
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(EnableContent))]
+    private bool _isAdministrator;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(EnableContent))]
+    private bool _hasCache = true;
+
+    public bool EnableContent => IsAdministrator && HasCache;
+
+    [ObservableProperty]
+    private uint _cacheSize;
+
+    [ObservableProperty]
+    private ObservableCollection<CacheInfoEx> _cacheElements = new();
+
+    private readonly ILogger<CachePageViewModel> _logger;
+    private readonly UACService _uacService;
+    private readonly IConfigurationManagerClientService _clientService;
+
+    public CachePageViewModel(ILogger<CachePageViewModel> logger, UACService uacService, IConfigurationManagerClientService clientService)
     {
-        [ObservableProperty]
-        private bool _isLoading = true;
+        _logger = logger;
+        _uacService = uacService;
+        _clientService = clientService;
 
-        public CachePage Page { get; set; }
+        IsAdministrator = _uacService.IsElevated;
 
-        [ObservableProperty]
-        [NotifyPropertyChangedFor(nameof(EnableContent))]
-        private bool _isAdministrator;
-
-        [ObservableProperty]
-        [NotifyPropertyChangedFor(nameof(EnableContent))]
-        private bool _hasCache = true;
-
-        public bool EnableContent => IsAdministrator && HasCache;
-
-        [ObservableProperty]
-        private uint _cacheSize;
-
-        [ObservableProperty]
-        private ObservableCollection<CacheInfoEx> _cacheElements = new();
-
-        private readonly ILogger<CachePageViewModel> _logger;
-        private readonly UACService _uacService;
-        private readonly IConfigurationManagerClientService _clientService;
-
-        public CachePageViewModel(ILogger<CachePageViewModel> logger, UACService uacService, IConfigurationManagerClientService clientService)
+        if(!IsAdministrator)
         {
-            _logger = logger;
-            _uacService = uacService;
-            _clientService = clientService;
+            return;
+        }
 
-            IsAdministrator = _uacService.IsElevated;
+        Task.Factory.StartNew(() => RefreshCache());
+    }
 
-            if(!IsAdministrator)
+    private void RefreshCache()
+    {
+        try
+        {
+            App.Current.DispatcherQueue.TryEnqueue(() =>
+            {
+                IsLoading = true;
+                CacheElements.Clear();
+            });
+
+            var clientConfig = _clientService.GetClientCacheConfig();
+            if (clientConfig != null)
+            {
+                App.Current.DispatcherQueue.TryEnqueue(() =>
+                {
+                    CacheSize = clientConfig.Size;
+                });
+            }
+
+            var cacheItems = _clientService.GetClientCache();
+            if (cacheItems == null)
             {
                 return;
             }
 
-            Task.Factory.StartNew(() => RefreshCache());
+            foreach (var element in cacheItems)
+            {
+                App.Current.DispatcherQueue.TryEnqueue(() =>
+                {
+                    CacheElements.Add(element);
+                });
+            }
+        }
+        catch(Exception ex)
+        {
+            _logger.LogWarning(ex, "Failed to update Cache");
+            HasCache = false;
+        }
+        finally
+        {
+            App.Current.DispatcherQueue.TryEnqueue(() =>
+            {
+                IsLoading = false;
+            });
+        }
+    }
+
+    [RelayCommand]
+    private void DeleteFromCache(string cacheElementId)
+    {
+        // TODO: Implement
+        //_clientService.DeleteFromCache(cacheElementId);
+
+        App.Current.DispatcherQueue.TryEnqueue(() =>
+        {
+            RefreshCache();
+        });
+    }
+
+    [RelayCommand]
+    private void RestartButton()
+    {
+        _uacService.RestartAsAdmin();
+    }
+
+    [RelayCommand]
+    private async void ChangeLocation()
+    {
+        //var openPicker = new FolderPicker()
+        //{
+        //    SuggestedStartLocation = PickerLocationId.ComputerFolder,
+        //    ViewMode = PickerViewMode.List
+        //};
+        //// Set options for your folder picker
+        //openPicker.FileTypeFilter.Add("*");
+
+        //// Retrieve the window handle (HWND) of the current WinUI 3 window.
+        //var window = App.Current.GetActiveWindow();
+        //var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
+
+        //// Initialize the folder picker with the window handle (HWND).
+        //WinRT.Interop.InitializeWithWindow.Initialize(openPicker, hWnd);
+
+        //// Open the picker for the user to pick a folder
+        //var folder = await openPicker.PickSingleFolderAsync();
+        //if (folder != null)
+        //{
+            
+        //}
+        //else
+        //{
+            
+        //}
+    }
+
+    [RelayCommand]
+    private async void DeleteFiles()
+    {
+        var dialog = new ContentDialog
+        {
+            XamlRoot = Page.XamlRoot,
+            Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style,
+            Title = "Delete Cache?",
+            PrimaryButtonText = "Delete",
+            CloseButtonText = "Cancel",
+            DefaultButton = ContentDialogButton.Close,
+            IsSecondaryButtonEnabled = false,
+            Content = new CacheDeleteDialog()
+        };
+
+        var result = await dialog.ShowAsync();
+        if(result == ContentDialogResult.Primary)
+        {
+            var deletePersistent = (dialog.Content as CacheDeleteDialog)!.DeletePersistentContent;
+        }
+    }
+
+    [RelayCommand]
+    private async void ApplyCacheSize()
+    {
+        if(CacheSize <= 0)
+        {
+            return;
         }
 
-        private void RefreshCache()
+        var cacheConfig = _clientService.GetClientCacheConfig();
+        if(cacheConfig == null || cacheConfig.Size == CacheSize)
+        {
+            return;
+        }
+
+        cacheConfig.Size = CacheSize;
+        cacheConfig = _clientService.PutInstance<CacheConfig>(cacheConfig, nameof(cacheConfig.Size));
+        CacheSize = cacheConfig.Size;
+
+        var dialog = new ContentDialog
+        {
+            XamlRoot = Page.XamlRoot,
+            Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style,
+            Title = "Restart Service?",
+            PrimaryButtonText = "Restart",
+            CloseButtonText = "Do not restart",
+            DefaultButton = ContentDialogButton.Close,
+            IsSecondaryButtonEnabled = false,
+            Content = new RestartServiceDialog()
+        };
+
+        var result = await dialog.ShowAsync();
+        if (result == ContentDialogResult.Primary)
         {
             try
             {
-                App.Current.DispatcherQueue.TryEnqueue(() =>
-                {
-                    IsLoading = true;
-                    CacheElements.Clear();
-                });
-
-                var clientConfig = _clientService.GetClientCacheConfig();
-                if (clientConfig != null)
-                {
-                    App.Current.DispatcherQueue.TryEnqueue(() =>
-                    {
-                        CacheSize = clientConfig.Size;
-                    });
-                }
-
-                var cacheItems = _clientService.GetClientCache();
-                if (cacheItems == null)
-                {
-                    return;
-                }
-
-                foreach (var element in cacheItems)
-                {
-                    App.Current.DispatcherQueue.TryEnqueue(() =>
-                    {
-                        CacheElements.Add(element);
-                    });
-                }
+                await _clientService.RestartServiceAsync();
             }
-            catch(Exception ex)
-            {
-                _logger.LogWarning(ex, "Failed to update Cache");
-                HasCache = false;
-            }
-            finally
-            {
-                App.Current.DispatcherQueue.TryEnqueue(() =>
-                {
-                    IsLoading = false;
-                });
-            }
-        }
-
-        [RelayCommand]
-        private void DeleteFromCache(string cacheElementId)
-        {
-            // TODO: Implement
-            //_clientService.DeleteFromCache(cacheElementId);
-
-            App.Current.DispatcherQueue.TryEnqueue(() =>
-            {
-                RefreshCache();
-            });
-        }
-
-        [RelayCommand]
-        private void RestartButton()
-        {
-            _uacService.RestartAsAdmin();
-        }
-
-        [RelayCommand]
-        private async void ChangeLocation()
-        {
-            //var openPicker = new FolderPicker()
-            //{
-            //    SuggestedStartLocation = PickerLocationId.ComputerFolder,
-            //    ViewMode = PickerViewMode.List
-            //};
-            //// Set options for your folder picker
-            //openPicker.FileTypeFilter.Add("*");
-
-            //// Retrieve the window handle (HWND) of the current WinUI 3 window.
-            //var window = App.Current.GetActiveWindow();
-            //var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
-
-            //// Initialize the folder picker with the window handle (HWND).
-            //WinRT.Interop.InitializeWithWindow.Initialize(openPicker, hWnd);
-
-            //// Open the picker for the user to pick a folder
-            //var folder = await openPicker.PickSingleFolderAsync();
-            //if (folder != null)
-            //{
-                
-            //}
-            //else
-            //{
-                
-            //}
-        }
-
-        [RelayCommand]
-        private async void DeleteFiles()
-        {
-            var dialog = new ContentDialog
-            {
-                XamlRoot = Page.XamlRoot,
-                Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style,
-                Title = "Delete Cache?",
-                PrimaryButtonText = "Delete",
-                CloseButtonText = "Cancel",
-                DefaultButton = ContentDialogButton.Close,
-                IsSecondaryButtonEnabled = false,
-                Content = new CacheDeleteDialog()
-            };
-
-            var result = await dialog.ShowAsync();
-            if(result == ContentDialogResult.Primary)
-            {
-                var deletePersistent = (dialog.Content as CacheDeleteDialog)!.DeletePersistentContent;
-            }
-        }
-
-        [RelayCommand]
-        private async void ApplyCacheSize()
-        {
-            if(CacheSize <= 0)
-            {
-                return;
-            }
-
-            var cacheConfig = _clientService.GetClientCacheConfig();
-            if(cacheConfig == null || cacheConfig.Size == CacheSize)
-            {
-                return;
-            }
-
-            cacheConfig.Size = CacheSize;
-            cacheConfig = _clientService.PutInstance<CacheConfig>(cacheConfig, nameof(cacheConfig.Size));
-            CacheSize = cacheConfig.Size;
-
-            var dialog = new ContentDialog
-            {
-                XamlRoot = Page.XamlRoot,
-                Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style,
-                Title = "Restart Service?",
-                PrimaryButtonText = "Restart",
-                CloseButtonText = "Do not restart",
-                DefaultButton = ContentDialogButton.Close,
-                IsSecondaryButtonEnabled = false,
-                Content = new RestartServiceDialog()
-            };
-
-            var result = await dialog.ShowAsync();
-            if (result == ContentDialogResult.Primary)
-            {
-                try
-                {
-                    await _clientService.RestartServiceAsync();
-                }
-                catch (TimeoutException) { }
-            }
+            catch (TimeoutException) { }
         }
     }
 }
